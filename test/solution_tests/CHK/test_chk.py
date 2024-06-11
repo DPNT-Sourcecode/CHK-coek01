024b79fbc6e72f58388e8f74eb239a742569d1fd
sbx_user1051 <sbx_user1051@169.254.85.253>
2024-06-11 19:58:16 +0000

    Tue Jun 11 19:58:16 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -7,3 +7,19 @@ from lib.solutions.CHK import checkout_solution
 class TestHello(TestCase):
     def test_all_items(self):
         self.assertEquals(115, checkout_solution.checkout("ABCD"))
+
+    def test_invalid_item(self):
+        self.assertEquals(-1, checkout_solution.checkout("ABCDF"))
+
+    def test_invalid_input(self):
+        self.assertEquals(-1, checkout_solution.checkout(10))
+
+    def test_empty_shopping_cart(self):
+        self.assertEquals(0, checkout_solution.checkout(""))
+
+    def test_promotions(self):
+        self.assertEquals(130, checkout_solution.checkout("AAA"))
+        self.assertEquals(260, checkout_solution.checkout("AAAAAA"))
+        self.assertEquals(180, checkout_solution.checkout("AAAA"))
+        self.assertEquals(190, checkout_solution.checkout("AAABBC"))
+
